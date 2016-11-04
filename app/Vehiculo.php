982553709d8379f3e27bafb84d4ac43a741e8620
Erick Joel Acuña Cotrina <erickjoelac@gmail.com>
2016-11-03 23:55:22 -0500

    Creando Modelos.
    - Se crean los dos modelos a utilizar.

---
+++ app/Vehiculo.php
@@ -0,0 +1,17 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vehiculo extends Model
+{
+	protected $table = 'vehiculo';
+
+	protected $primaryKey = 'serie';
+
+	protected $fillable = array('color', 'cilindraje', 'potencia', 'peso');
+
+	public function fabricante()
+	{
+		this->belongsTo('Fabricante');
+	}
+}
