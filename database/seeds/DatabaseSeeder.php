0703c62da457bdc3ffbe3d83983127b75f117132
Erick Joel Acuña Cotrina <erickjoelac@gmail.com>
2016-11-04 20:32:21 -0500

    Se hace el sistema de autenticación e inserción.
    - Se crea el modelo y se usa el middleware de autenticación básica.
    - Se implementan los métodos store para vechículos y fabricantes.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+
 class DatabaseSeeder extends Seeder {
 
 	/**
@@ -15,6 +17,9 @@ class DatabaseSeeder extends Seeder {
 
 		$this->call('FabricanteSeeder');
 		$this->call('VehiculoSeeder');
+
+		User::truncate();
+		$this->call('UserSeeder');
 	}
 
 }
